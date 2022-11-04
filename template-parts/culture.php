adb46789af44d0944a8813eed1ed7b4a14a57ce9
Pablo ST <pablost88@gmail.com>
2022-11-04 13:30:21 -0300

    culture section > css fixes

--- template-parts/culture.php
+++ template-parts/culture.php
@@ -1,31 +1,33 @@
 <section id="culture" class="culture">
-	<div class="culture__decoration culture-decoration" aria-hidden="true">
-		<div class="culture-decoration__label">
-			<span class="culture-decoration__label-inner">Company</span>
+	<div class="culture__wrapper">
+		<div class="culture__decoration culture-decoration" aria-hidden="true">
+			<div class="culture-decoration__label">
+				<span class="culture-decoration__label-inner">Company</span>
+			</div>
+			<div class="culture-decoration__label">
+				<span class="culture-decoration__label-inner">Culture</span>
+			</div>
+			<span class="culture-decoration__number">002</span>
 		</div>
-		<div class="culture-decoration__label">
-			<span class="culture-decoration__label-inner">Culture</span>
-		</div>
-		<span class="culture-decoration__number">002</span>
-	</div>
-	<div class="culture__media">
-		<div class="culture__media-main">
-			<picture class="culture__picture culture__picture-main">
-				<img src="dist/images/culture/main.jpg" alt="" class="culture__image">
+		<div class="culture__media">
+			<div class="culture__media-main">
+				<picture class="culture__picture culture__picture-main">
+					<img src="dist/images/culture/main.jpg" alt="" class="culture__image">
+				</picture>
+			</div>
+			<picture class="culture__picture culture__picture-top">
+				<img src="dist/images/culture/top.jpg" alt="" class="culture__image">
+			</picture>
+			<picture class="culture__picture culture__picture-side">
+				<img src="dist/images/culture/side.jpg" alt="" class="culture__image">
 			</picture>
 		</div>
-		<picture class="culture__picture culture__picture-top">
-			<img src="dist/images/culture/top.jpg" alt="" class="culture__image">
-		</picture>
-		<picture class="culture__picture culture__picture-side">
-			<img src="dist/images/culture/side.jpg" alt="" class="culture__image">
-		</picture>
-	</div>
-	<div class="culture__content culture-content">
-		<header class="culture__header culture-header">
-			<span class="culture-header__subtitle">People Making A Difference</span>
-			<h1 class="culture-header__title">Company Culture</h1>
-		</header>
-		<p class="feature-content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<div class="culture__content culture-content">
+			<header class="culture__header culture-header">
+				<span class="culture-header__subtitle">People Making A Difference</span>
+				<h1 class="culture-header__title">Company Culture</h1>
+			</header>
+			<p class="feature-content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
 	</div>
 </section>
